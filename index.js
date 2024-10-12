62f54a874587a57b5bfdc5876c4f944d5132996a
GoaSoftwareDevelopment <goasoftwaredevelopment@gmail.com>
2024-10-12 16:41:31 +0400

    Mongodb added

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from "dotenv";
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 import studentRouter from './routes/student.router.js';
+
 
 const app = express();
 
@@ -15,7 +17,16 @@ app.use(cors());
 // Routes
 app.use('/api/student', studentRouter);
 
+// Connect mongodb
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log("Connected to MongoDB"); 
+        app.listen(process.env.PORT, () => {
+            console.log("Listening on port 3000");
+        });
+    })
+    .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+    });
 
-app.listen(process.env.PORT, () => {
-    console.log("Listening on port 3000");
-});
+
--- model/leader.model.js
+++ model/leader.model.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const LeaderSchema = new mongoose.Schema({
     leaderEmail: {
